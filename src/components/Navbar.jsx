e0ef206e368bfb23e51e199978d4cfb0cc0b8066
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:22:17 +0530

    feat: add Navbar with nav links and wallet button

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { NavLink, Link } from 'react-router-dom';
+import WalletButton from './WalletButton.jsx';
+import './Navbar.css';
+
+/**
+ * Top navigation bar with brand, primary links and the wallet button.
+ */
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__inner">
+        <Link to="/" className="navbar__brand">
+          <span className="navbar__logo" aria-hidden="true">
+            💧
+          </span>
+          StreamPay
+        </Link>
+
+        <nav className="navbar__links">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/create">Create Stream</NavLink>
+        </nav>
+
+        <WalletButton />
+      </div>
+    </header>
+  );
+}
